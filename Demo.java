49e7c72b33ff9dab630a2424249fe5fe4d6dd069
Wilssoooonnn <adamnuralifi11@gmail.com>
2024-11-13 10:17:21 +0700

    add langkah 8-10 2.4

--- Demo.java
+++ Demo.java
@@ -34,11 +34,20 @@ public class Demo {
         // pegawai1.mengajar();
 
         // LANGKAH 6
+        
+        // TenagaPendidikan test = (TenagaPendidikan) pegawai1;
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom, M.Kom", "199402");
+        
         Pegawai pegawai1 = dosen1;
         
         System.out.println(pegawai1.nip);
         System.out.println(pegawai1.nama);
         pegawai1.displayInfo();
+        
+        Dosen newDosen = (Dosen) pegawai1;
+        
+        System.out.println(newDosen.nama);
+        System.out.println(newDosen.nidn);
+        newDosen.mengajar();
     }
 }
